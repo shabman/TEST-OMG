13ddfb2edf024309f79dd531357f7fb2580a778d
shabman <59781727+shabman@users.noreply.github.com>
2020-08-23 18:58:04 +0100

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,1 +1,14 @@
-print("Hi")
+## why do i do stuff ##
+
+
+class HelloWorld():
+    
+    @property
+    def hello_world(self):
+      print("Hello world!")
+      
+     # ok
+    
+hello_World = HelloWorld()
+
+hello_World.hello_world
